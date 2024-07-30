23e5ac2ab147e3fefce274f4f74b5074390dda38
Yasi-Mortazavii <mortazaviiyasi@gmail.com>
2024-07-30 06:17:29 +0330

    Add project files.

---
+++ DapperApp/Controllers/CustomerController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace DapperApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        // GET: CustomersController
+        public ActionResult Index()
+        {
+            //_customerRepository.Add();
+            _customerRepository.Add( new CuustomerDto
+            {
+                Name = "test",
+                LastName = "test2"
+            });
+            return View();
+        }
+
+        // GET: CustomersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                //_customerRepository.Add();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+    
+}
---
+++ Repository/CustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Repository
+{
+    public interface ICustomerRepository
+    {
+        int Add(CuustomerDto customer);
+    
+    }
+    public class CustomerRepository: ICustomerRepository
+    {
+        private readonly string  ConnectionString = @"server = DESKTOP-4PP4QV0; Initial Catalog = DapperDb; integrated security =True;"; 
+
+        public int Add(CuustomerDto customer)
+        {
+            string sql = "INSERT INTO Customers (Name, LastName) Values (@Name, @LastName)";
+            var connection = new SqlConnection (ConnectionString);
+            var Result = connection.Execute(sql, new { Name = customer.Name, LastName = customer.LastName});
+            return Result;
+        }
+    }
+    public class CuustomerDto
+    {
+        public long id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
